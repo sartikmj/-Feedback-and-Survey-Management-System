f6cd78476924d63105be510300325e28901a01af
sartikmj <sartikmj@gmail.com>
2025-07-15 09:39:11 +0530

    Backend done and Reorganized project into Frontend and Backend folders

---
+++ Backend/server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); // Importing the CORS middleware to handle cross-origin requests
+
+const app = express();
+
+app.use(cors()); //adds the CORS (Cross-Origin Resource Sharing) middleware to 
+// your Express app. This allows your server to accept requests from web 
+// applications hosted on different domains or ports, which is essential for 
+// enabling communication between your backend and frontend if they are served 
+// separately.
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/feedback-app',{
+}).then(()=> console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+//Schema of the database for feedback
+const feedbackSchema = new mongoose.Schema({
+    studentName: {
+        type:String,
+        default: "Anonymous"
+    },
+
+    email: {
+        type:String,
+        default: ''
+    },
+
+    subject: {
+        type:String,
+        required: true
+    },
+
+    teacherName: {
+        type:String,
+        required: true
+    },
+
+    rating: {
+        type:Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+
+    comment:{
+        type:String,
+        default: ''
+    },
+
+    date:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+//Exporting the schema as a model
+const Feedback = mongoose.model('Feedback', feedbackSchema);
+
+//where to submit the data from backend
+app.post('/submit-feedback' /*name of the api where data will be send */, async (req,res)=>{
+    try{
+        const data = req.body; //get the data from the request body
+
+        if(!data.studentName || studentName.trim()===''){
+            data.studentName = "Anonymous"; //if student name is not provided, "Anonymouse" will be set to the database
+        }
+
+        const feedback = new Feedback(data); //create a new instance of the Feedback model with the data
+        await feedback.save(); //save the feedback to the database
+        res.status(201).json({ message: "Feedback submitted successfully!" });
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong !" });
+    }
+})
+
+const PORT = 5000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
--- Frontend/src/components/Form.jsx
+++ Frontend/src/components/Form.jsx
@@ -19,6 +19,8 @@ const Form = () => {
   }
 
   const handleSubmit = async (e) =>{
+    const payload = { ...form, rating: Number(form.rating) }; //converting the rating into Number from string
+    //Because form inputs in HTML always return values as strings, even if the input type is number.
     e.preventDefault();
     // Here we can send the form data to the backend API
     try{
@@ -43,7 +45,10 @@ const Form = () => {
           <div className="text-center">
           <h2 className="text-2xl font-bold text-green-600 mb-2">Thank You!</h2>
           <p className="text-gray-600">Your Feedback has been submitted successfully !</p>
-          <button className="mt-6 px-4 py-2 bg-blue-500 text-white rounded">Submit Another Feedback</button>
+          <button 
+          className="mt-6 px-4 py-2 bg-blue-500 text-white rounded"
+          onClick={()=> setSubmitted(false)}
+          >Submit Another Feedback</button>
         </div>
         ) : (
           <>
@@ -80,7 +85,7 @@ const Form = () => {
             Rating(1 to 5)
           </label>
           <select name="rating" required value={form.rating} onChange={handleChange}>
-            <option>Select Rating</option>
+            <option value="">Select Rating</option>
             {[1, 2, 3, 4, 5].map((rating) => (
               <option key={rating} value={rating}>
                 {rating}
